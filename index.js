8810357fdd175aad07b5f8d2c93345ab98738bc8
mturlaev <muhammadturlaev@gmail.com>
2022-05-18 10:44:35 +0300

    создал объект со значениями

---
+++ index.js
@@ -0,0 +1,12 @@
+const soldier = {
+    name: "maga",
+    health: 10,
+    weapon: {
+        weeaponName: "ak-47",
+        numberOfRounds: 30,
+    },
+    supplies: 3,
+    shoot: function () {},
+    recharge: function () {},
+    hurt: function () {},
+}
