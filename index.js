27c6069e7304dfdb2e42dc9813ad1dba9ef36a21
mturlaev <muhammadturlaev@gmail.com>
2022-05-18 11:48:56 +0300

    работа с методами

--- index.js
+++ index.js
@@ -6,8 +6,30 @@ const soldier = {
         numberOfRounds: 30,
     },
     supplies: 3,
-    shoot: function () {},
-    recharge: function () {},
-    hurt: function () {},
-    
+    shoot: function () {
+        if (this.weapon.numberOfRounds !== 0){
+            this.weapon.numberOfRounds -= 1;
+            console.log('ба-бах');
+        } else if (this.weapon.numberOfRounds === 0){
+            console.log('обойма пуста. Перезаредитесь') 
+        }
+    },
+    recharge: function () {
+        if (this.weapon.numberOfRounds === 0) {
+            this.weapon.numberOfRounds += 30;
+        } else if (this.weapon.numberOfRounds === 30) {
+                this.supplies--;
+                console.log('Перезарядка...')
+            } else if (this.supplies === 0) {
+                console.log('Ни осталось припасов')
+            } return
+        },
+    hurt: function () {
+        if (this.health === 0){
+            console.log('Ты проиграл')
+        } else if (this.health--) {
+            console.log('Ты ранен')
+        }
+    },
 }
+
